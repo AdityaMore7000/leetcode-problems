8891df825eef8b3ec64cacba37fe8fa142d501c2
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-04-07 17:55:58 +0530

    Time: 3 ms (45.50%) | Memory: 7.3 MB (75.32%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int leftMin = 0, leftMax = 0;
+
+        for (char c : s) {
+            if (c == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (c == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMax < 0) return false;
+            if (leftMin < 0) leftMin = 0;
+        }
+        
+        return leftMin == 0;
+    }
+};
