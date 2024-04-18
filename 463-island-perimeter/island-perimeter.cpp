221f1a1f5d97539782e30e549009add1001acf95
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-04-18 22:46:08 +0530

    Time: 62 ms (89.91%) | Memory: 100.8 MB (48.27%) - LeetSync

---
+++ 463-island-perimeter/island-perimeter.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        int perimeter = 0;
+        int rows = grid.size();
+        int cols = grid[0].size();
+        
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == 1) {
+                    perimeter += 4;
+                    if (r > 0 && grid[r-1][c] == 1) {
+                        perimeter -= 2;
+                    }
+                    if (c > 0 && grid[r][c-1] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+        
+        return perimeter;
+    }
+};
