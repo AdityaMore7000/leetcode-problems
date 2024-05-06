81ff334778a87a4fc99bbd9aa7a6b5051c727164
Aditya More <99179330+AdityaMore7000@users.noreply.github.com>
2024-05-06 10:02:45 +0530

    Time: 199 ms (99.67%) | Memory: 159.7 MB (90.66%) - LeetSync

---
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* current = head;
+
+        while (current != nullptr) {
+            ListNode* nextNode = current->next;
+            current->next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev;
+    }
+
+    ListNode* removeNodes(ListNode* head) {
+        cin.tie(nullptr);
+        cout.tie(nullptr);
+        ios_base::sync_with_stdio(false);
+        ListNode* reversedHead = reverseList(head);
+
+        ListNode* current = reversedHead;
+        int maxValue = INT_MIN;
+        ListNode* prev = nullptr;
+
+        while (current != nullptr) {
+            if (current->val < maxValue) {
+                prev->next = current->next;
+            } else {
+                maxValue = current->val;
+                prev = current;
+            }
+            current = current->next;
+        }
+
+        return reverseList(reversedHead);
+    }
+};
